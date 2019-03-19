68a3d04a97d29dfad039023320506dec37187af9
Danielle Mizrachi <dmiz@brandeis.edu>
2019-03-19 18:05:43 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(parrotdefault="Squawk!")
-  puts parrotdefault
+def parrot(phrase="Squawk!")
+  puts phrase
   return
 end 
